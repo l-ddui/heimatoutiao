662bd7b05c84f3940b7923b671170a2297648092
l-dddui <lzekai@foxmail.com>
2021-04-20 09:33:24 +0800

    调整项目初始化结构和代码

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <template>
-  <div id="app">
-  </div>
+  <div id="app">项目开始</div>
 </template>
 
 <script>
-
 export default {
-  name: 'App'
-}
+  name: "App",
+};
 </script>
 
 <style lang="less">
